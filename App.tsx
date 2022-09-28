4b633b9e6cfc3651f2e48342598d81be7b9be6e7
Ahmed <ahmed_5g@yahoo.com>
2022-09-28 06:55:14 +0200

    working

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MedButton from "./components/mini/MedButton";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontSize: 34, color: "black", fontWeight: "bold" }}>
+        Buttons
+      </Text>
+      <MedButton
+        onPress={() => console.log("hi")}
+        width={150}
+        style={{ elevation: 10 }}
+        borderRadius={20}
+        color="red"
+        title="Icon"
+        textStyle={{ fontSize: 29 }}
+        icon={<AntDesign name="rightcircleo" size={24} color="black" />}
+        iconContainerStyle={{ marginLeft: 20 }}
+      />
+      <MedButton onPress={() => {}} width={190} title="Disabled" disabled />
+      <MedButton
+        onPress={() => {}}
+        width={90}
+        square
+        title="Square"
+        textStyle={{ fontSize: 18 }}
+      />
+      <MedButton onPress={() => {}} circle title="circle" />
+      <MedButton
+        onPress={() => {}}
+        width={100}
+        loading={<Text>Loading</Text>}
+        square
+        style={{ marginTop: 20 }}
+        title="Square"
+      />
+      <MedButton onPress={() => {}} width={100} loading color={"green"} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Wallet App UI
+
+- React native Expo
+- Typescript
+- Custom components
+
+# Installation
+
+```
+git clone
+cd component
+yarn
+expo start
+```
+
+# Screenshots
+
+<img src="./screenshots/Screenshot_1664340693.png" alt="Screen" width="300" height="700"/>
